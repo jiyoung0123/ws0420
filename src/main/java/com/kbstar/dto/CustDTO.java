6c21fa24dc9e53f6f191df50167830db4bb3ec9a
jiyoung0123 <taebin100@gmail.com>
2023-04-20 17:08:43 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/dto/CustDTO.java
@@ -0,0 +1,20 @@
+package com.kbstar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@ToString
+
+
+public class CustDTO {
+    private String id;
+    private String pwd;
+    private String name;
+
+
+}
